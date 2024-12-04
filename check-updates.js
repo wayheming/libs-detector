6a9b685b48443cf21ff70120d3a13c2a182fed65
Ernest Behinov <31981892+wayheming@users.noreply.github.com>
2024-12-04 11:03:40 +0100

    Update check-updates.js

--- check-updates.js
+++ check-updates.js
@@ -101,6 +101,8 @@ async function callGPTAPI(description, repo, version, url) {
 
   const json = await response.json();
   const structuredOutput = json.choices[0].message.content;
+
+  console.log(structuredOutput);
   
   return JSON.parse(structuredOutput);
 }
